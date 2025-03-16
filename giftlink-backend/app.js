b1e15c135cdc87b7d8a295fb41e293c9c98c65b1
raulchavezjr7 <raulchavezjr7@gmail.com>
2025-03-15 23:32:28 -0400

    adjusted code positioning

--- giftlink-backend/app.js
+++ giftlink-backend/app.js
@@ -26,7 +26,7 @@ app.use(express.json());
     const giftRoutes = require('./routes/giftRoutes')
 
 // Search API Task 1: import the searchRoutes and store in a constant called searchRoutes
-    app.use('/api/gifts', giftRoutes)
+    //{{insert code here}}
 
 
 const pinoHttp = require('pino-http');
@@ -36,7 +36,7 @@ app.use(pinoHttp({ logger }));
 
 // Use Routes
 // Gift API Task 2: add the giftRoutes to the server by using the app.use() method.
-//{{insert code here}}
+    app.use('/api/gifts', giftRoutes)
 
 // Search API Task 2: add the searchRoutes to the server by using the app.use() method.
 //{{insert code here}}
